3fd456af722c4bd5a37021d08c8a327551e1f766
Isaac <isaacspencer17@gmail.com>
2025-01-24 01:40:58 -0700

    removed main site

--- src/logic/formatDate.ts
+++
@@ -1,4 +0,0 @@
-export function formatDate(date: string) {
-  const options: Intl.DateTimeFormatOptions = { year: 'numeric', month: 'long', day: 'numeric' }
-  return new Date(date).toLocaleDateString(undefined, options)
-}
